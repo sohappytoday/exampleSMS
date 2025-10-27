6e813118091ab4fe9d9a559c47ca8b534d93dca9
geooeg <kgeo6264@gmail.com>
2025-10-28 01:00:51 +0900

    chore: initialize Spring Boot project with basic dependencies

---
+++ src/main/java/com/example/exampleSMS/user/domain/User.java
@@ -0,0 +1,11 @@
+package com.example.exampleSMS.user.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Getter
+@NoArgsConstructor
+public class User {
+}
