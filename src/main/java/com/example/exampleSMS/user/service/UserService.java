980b7fcc1864e1c96ac9961fc5858596aa97953e
geoKim <kgeo6264@gmail.com>
2025-10-28 18:32:11 +0900

    feat: Implement register user logic

--- src/main/java/com/example/exampleSMS/user/service/UserService.java
+++ src/main/java/com/example/exampleSMS/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.exampleSMS.user.service;
 
+import com.example.exampleSMS.user.domain.User;
 import com.example.exampleSMS.user.dto.request.RegisterUserRequest;
 import com.example.exampleSMS.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,10 @@ public class UserService {
     private final UserRepository userRepository;
 
     public void registerUser(RegisterUserRequest registerUserRequest){
+        String username = registerUserRequest.getUsername();
+        String phoneNumber = registerUserRequest.getPhoneNumber();
 
+        User user = User.create(username,phoneNumber);
+        userRepository.save(user);
     }
 }
